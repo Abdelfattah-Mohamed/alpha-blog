665ea5e0d300779e9145028bb32f9f879d0a67bf
Abdelfattah Mohamed <abdelftahmohamed30@gmail.com>
2019-03-28 10:25:53 +0000

    correct username at Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20190327210724) do
-=======
-ActiveRecord::Schema.define(version: 20190203072523) do
->>>>>>> parent of 8aec76f... add users
+ActiveRecord::Schema.define(version: 20190328101846) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +19,13 @@ ActiveRecord::Schema.define(version: 20190203072523) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
